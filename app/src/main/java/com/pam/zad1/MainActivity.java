f25b31e6555d56edd18a711c1a71e8dc0ccfc55a
czerwonogrodzki <czerwonogrodzkipatryk@gmail.com>
2024-03-08 21:41:35 +0100

    Removed unnecessary stuff, including SecondFragment and lorem ipsum, changed next page button into clicker counter

--- app/src/main/java/com/pam/zad1/MainActivity.java
+++ app/src/main/java/com/pam/zad1/MainActivity.java
@@ -17,12 +17,15 @@ import com.pam.zad1.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+    private Button button;
+    private int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,18 +35,19 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
+        button = (Button) findViewById(R.id.button_first);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                counter++;
+                button.setText("Naciśnięto x" + counter);
+            }
+        });
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
